fbec149948d4e26a2496fdcd7bfb1b8c4ba97918
kazuma <kazumazuma1124@gmail.com>
2019-11-02 12:20:56 +0900

    first commit

---
+++ neural_network/neuron_output.py
@@ -0,0 +1,28 @@
+# ニューロン
+class Neuron:
+    input_sum = 0.0
+    output = 0.0
+
+    def setInput(self, inp):
+        self.input_sum += inp
+
+    def getOutput(self):
+        self.output = self.input_sum
+        return self.output
+
+# ニューラルネットワーク
+class NeuralNetwork:
+    neuron = Neuron()
+
+    # 実行
+    def commit(self, input_data):
+        for data in input_data:
+            self.neuron.setInput(data)
+        return self.neuron.getOutput()
+
+# ニューラルネットワークのインスタンス
+nurral_network = NeuralNetwork()
+
+# 実行
+trial_data = [1.0, 2.0, 3.0]
+print(nurral_network.commit(trial_data))
